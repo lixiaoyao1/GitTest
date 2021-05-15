a2031b63d119351c842f37bf904c0c9923d54dfa
wp <wp@qq.com>
2021-05-15 10:03:00 +0800

    second commit

--- src/main/java/com/wp/Ran.java
+++ src/main/java/com/wp/Ran.java
@@ -2,7 +2,7 @@ package com.wp;
 
 public class Ran {
     public static void main(String[] args) {
-        String str = "100.50.05";
+        String str = "100.50.05,0.0";
         String[] strs = str.split("\\.");
 
         for(String s : strs){
