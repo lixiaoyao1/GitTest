3090325e64110a6207e48fc378d8999fc8f15a12
wp <wp@qq.com>
2021-05-15 10:00:45 +0800

    first commit

---
+++ src/main/java/com/wp/Ran.java
@@ -0,0 +1,15 @@
+package com.wp;
+
+public class Ran {
+    public static void main(String[] args) {
+        String str = "100.50.05";
+        String[] strs = str.split("\\.");
+
+        for(String s : strs){
+            System.out.println(s);
+        }
+
+        int num = Integer.valueOf("00100");
+        System.out.println(num);
+    }
+}
